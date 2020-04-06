246eeac31fb0b6e0c5bb2dff4c331e96b19d38f3
akhileshiiestan <56578793+akhileshiiestan@users.noreply.github.com>
2020-04-06 14:07:14 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,9 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Jenkins build trigeered by using git hub configuration");
+		System.err.println("This is my second jenkins job");
+	}
+
+}
